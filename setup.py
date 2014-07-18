d6c8f40c3b785371777d8069322bfed963fcc398
Renat Galimov <renat2017@gmail.com>
2014-07-18 11:07:00 +0400

    Added zip_safe option to setup. Bumped version to 1.5.10-r1

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
+'''Django package for jquery-colorbox:
+A lightweight customizable lightbox plugin for jQuery'''
+
 setup(
     name='django-colorbox',
-    version='1.5.10',
+    version='1.5.10-r1',
     url='https://github.com/renat2017/django-colorbox',
-    description='Django package for jquery-colorbox: A lightweight customizable lightbox plugin for jQuery',
+    description=locals()['__doc__'],
     author='Jack Moore',
     maintainer='Renat Galimov',
     maintainer_email='renat2017@gmail.com',
     license='MIT License',
-    keywords=['django', 'jquery', 'lightbox', 'staticfiles', 'overlay', 'image'],
+    keywords=['django', 'jquery', 'lightbox',
+              'staticfiles', 'overlay', 'image'],
     platforms='any',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -22,5 +26,15 @@ setup(
         'Topic :: Utilities',
     ],
     packages=['django_colorbox'],
-    package_data={'django_colorbox': ['static/js/django_colorbox/*.js', 'static/js/django_colorbox/i18n/*.js']}
+    zip_safe=False,
+    package_data={
+        'django_colorbox': [
+            'static/js/django_colorbox/*.js',
+            'static/js/django_colorbox/i18n/*.js'
+        ]
+    },
+    install_requires=(
+        'Django'
+    )
+
 )
